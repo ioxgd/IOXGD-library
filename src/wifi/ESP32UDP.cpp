650520a86bb83f05a720fac2b99d34f13b21bf40
Max <max30012540@hotmail.com>
2019-11-29 14:35:01 +0700

    Update UDP

--- src/wifi/ESP32UDP.cpp
+++ src/wifi/ESP32UDP.cpp
@@ -51,7 +51,54 @@ int WiFiUDP::beginPacket(String host, uint16_t port) {
 }
 
 int WiFiUDP::endPacket() {
+	clearBuffer();
 
+	// Serial.println("Send udp send");
+  
+	SERIAL_ESP.write(0x1F); // Start 1
+	SERIAL_ESP.write(0xF1); // Start 2
+	SERIAL_ESP.write(0x17); // UDP send command
+	SERIAL_ESP.write((uint8_t)this->packetHost.length());
+	SERIAL_ESP.print(this->packetHost);
+	SERIAL_ESP.write((uint8_t)(port >> 8));
+	SERIAL_ESP.write((uint8_t)(port & 0xFF));
+	SERIAL_ESP.write((uint8_t)(writePointer & 0xFF));
+	SERIAL_ESP.write(writeBuffer, writePointer);
+
+	int state = 0;
+
+	// Check Start
+	while (1) {
+		if (SERIAL_ESP.available()) {
+			uint8_t c = SERIAL_ESP.read();
+			if (state == 0) {
+				if (c == 0x1F) {
+					state = 1;
+				}
+			} else if (state == 1) {
+				if (c == 0xF1) {
+					state = 2;
+				} else {
+					state = 0;
+				}
+			} else if (state == 2) {
+				if (c == 0x17) { // UDP send response ?
+					break;
+				} else {
+					state = 0;
+				}
+			}
+		} else {
+			delay(1);
+		}
+	}
+
+	uint8_t writeSize = writePointer & 0xFF;
+
+	free(writeBuffer);
+	writePointer = 0;
+
+	return writeSize;
 }
 
 size_t WiFiUDP::write(uint8_t data) {
@@ -64,20 +111,95 @@ size_t WiFiUDP::write(uint8_t *buffer, size_t size) {
 }
 
 int WiFiUDP::parsePacket() {
+	if (readBuffer) {
+		free(readBuffer);
+		readBuffer = NULL;
+	}
 
+	clearBuffer();
+
+	// Serial.println("Send udp parse packet");
+  
+	SERIAL_ESP.write(0x1F); // Start 1
+	SERIAL_ESP.write(0xF1); // Start 2
+	SERIAL_ESP.write(0x18); // UDP parse packet command
+	SERIAL_ESP.write(0x01); // Dummy data
+
+	int state = 0;
+
+	// Check Start
+	while (1) {
+		if (SERIAL_ESP.available()) {
+			uint8_t c = SERIAL_ESP.read();
+			if (state == 0) {
+				if (c == 0x1F) {
+					state = 1;
+				}
+			} else if (state == 1) {
+				if (c == 0xF1) {
+					state = 2;
+				} else {
+					state = 0;
+				}
+			} else if (state == 2) {
+				if (c == 0x18) { // UDP parse packet response ?
+					break;
+				} else {
+					state = 0;
+				}
+			}
+		} else {
+			delay(1);
+		}
+	}
+
+	while (SERIAL_ESP.available() < 2) delay(1);
+
+	readBufferLength = (uint16_t)(SERIAL_ESP.read()) << 8;
+	readBufferLength |= (uint16_t)(SERIAL_ESP.read()) & 0xFF;
+
+	if (readBufferLength > 0) {
+		SERIAL_ESP.readBytes(_remoteIP, 4);
+
+		while (SERIAL_ESP.available() < 2) delay(1);
+
+		_remotePort = (uint16_t)(SERIAL_ESP.read()) << 8;
+		_remotePort |= (uint16_t)(SERIAL_ESP.read()) & 0xFF;
+
+		readPointer = 0;
+		readBuffer = (uint8_t*)malloc(readBufferLength);
+
+		SERIAL_ESP.readBytes(readBuffer, readBufferLength);
+	}
+
+	return readBufferLength;
 }
 
 int WiFiUDP::available() {
-
+	return readBufferLength;
 }
 
 int WiFiUDP::read() {
-    uint8_t c;
-    read(&c, 1);
+    if (readBufferLength == 0) {
+		return -1; 
+	}
+
+	uint8_t c = readBuffer[readPointer];
+	readPointer++;
+	readBufferLength--;
+
     return c;
 }
 
-int WiFiUDP::read(uint8_t* buffer, size_t len) ;
+int WiFiUDP::read(uint8_t* buffer, size_t len) {
+	if (readBufferLength == 0) {
+		return -1; 
+	}
+
+	memcpy(buffer, &readBuffer[readPointer], len);
+	readPointer += len;
+	readBufferLength -= len;
+}
 
 int WiFiUDP::peek() {
     // Not support
--- src/wifi/ESP32UDP.h
+++ src/wifi/ESP32UDP.h
@@ -4,11 +4,11 @@
 #include "Arduino.h"
 #include "ESP32.h"
 
-#define WRITE_BUFFER_SIZE 8192
+#define WRITE_BUFFER_SIZE 256
 
 class WiFiUDP : public Stream {
 	private:
-		IPAddress _remoteIP;
+		uint8_t _remoteIP[4] = { 0, 0, 0, 0 };
 		uint16_t _remotePort;
 
 		String packetHost = "";
@@ -18,22 +18,28 @@ class WiFiUDP : public Stream {
 		uint8_t *readBuffer = NULL;
 
 		uint16_t writePointer = 0;
+		uint16_t readPointer = 0;
+		uint16_t readBufferLength = 0;
 
 	public:
 		WiFiUDP();
 
 		uint8_t begin(uint16_t p) ;
 		int beginPacket(String host, uint16_t port) ;
-		int endPacket() ;
 		size_t write(uint8_t) ;
 		size_t write(uint8_t *buffer, size_t size) ;
+		int endPacket() ;
+
 		int parsePacket() ;
 		int available() ;
 		int read() ;
 		int read(uint8_t* buffer, size_t len) ;
+
+		// Not support
 		int peek() ;
 		void flush() ;
-		IPAddress remoteIP() { return _remoteIP; };
+
+		IPAddress remoteIP() { return IPAddress(_remoteIP[0], _remoteIP[1], _remoteIP[2], _remoteIP[3]); };
 		uint16_t remotePort() { return _remotePort };
 
 };
